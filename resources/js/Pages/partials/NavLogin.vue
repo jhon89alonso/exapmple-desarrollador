733cbc63fa389d113825427b59142e4d04bd8c43
Jhon Anderson Alonso <programador2@impsercom.com.co>
2023-08-16 10:51:47 -0500

    creacion de componentes y instalacion o actualizacion de paquetes en package.json y composer.json

---
+++ resources/js/Pages/partials/NavLogin.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+        <Link
+            v-if="$page.props.auth.user"
+            :href="route('dashboard')"
+            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+            >Dashboard</Link
+        >
+
+        <template v-else>
+            <Link
+                :href="route('login')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Ingresar</Link
+            >
+
+            <Link
+                v-if="canRegister"
+                :href="route('register')"
+                class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Registrarse</Link
+            >
+        </template>
+    </div>
+    <!-- <div class=" text-white">test</div> -->
+</template>
+
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+});
+</script>
