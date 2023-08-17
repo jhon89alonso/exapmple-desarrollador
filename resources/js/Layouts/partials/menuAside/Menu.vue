80d36d08131ead503f2667d4daf863f0013727b2
Jhon Anderson Alonso <programador2@impsercom.com.co>
2023-08-17 11:12:40 -0500

    cambios en componentes  y en package.json

--- resources/js/Layouts/partials/menuAside/Menu.vue
+++ resources/js/Layouts/partials/menuAside/Menu.vue
@@ -1,6 +1,7 @@
 <template>
     <aside class="left-0">
         <div
+            v-show="menuAside"
             class="bg-gray-400 lg:py-2 lg:pl-2 w-60 fixed z-40 top-0 h-screen transition-position overflow-hidden"
         >
             <div class="flex flex-row h-14 items-center justify-between">
@@ -9,10 +10,32 @@
                 >
                     <b class="font-black">Menu</b>
                 </div>
-                <button class="lg:inline-block p-3 bg-white">close</button>
+                <button
+                    @click="closeAsideBar"
+                    class="lg:inline-block p-3 bg-black text-white"
+                >
+                    close
+                </button>
             </div>
         </div>
     </aside>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, defineEmits, defineExpose } from "vue";
+
+const emit = defineEmits(["open-aside-bar", "close-aside-bar"]);
+const menuAside = ref(false);
+
+const openAsideBar = (event) => {
+    menuAside.value = true;
+    emit("open-aside-bar", event);
+};
+const closeAsideBar = (event) => {
+    menuAside.value = false;
+    emit("close-aside-bar", event);
+};
+
+// Exporta el método openAsideBar para que esté disponible en el componente padre
+defineExpose({menuAside, openAsideBar});
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,7 +12,7 @@ import { Head } from '@inertiajs/vue3';
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        <Menu />
+        
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
