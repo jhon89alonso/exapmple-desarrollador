2fd0b8865356921e6c9101fdeb1ba4e0635a1770
Jhon Anderson Alonso <programador2@impsercom.com.co>
2023-09-01 15:13:07 -0500

    creacion de modelo.migracion,requests y controller de sectionexternalsheets

---
+++ app/Http/Controllers/SectionExternalSheetsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contents\SectionExternalSheets;
+use App\Http\Requests\StoreSectionExternalSheetsRequest;
+use App\Http\Requests\UpdateSectionExternalSheetsRequest;
+
+class SectionExternalSheetsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSectionExternalSheetsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSectionExternalSheetsRequest $request, SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+}
---
+++ app/Models/Contents/SectionExternalSheets.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Contents;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SectionExternalSheets extends Model
+{
+    use HasFactory; use SoftDeletes;
+    
+}
