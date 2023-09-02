d5429c3b4be94d17c000f582d859988d307985db
Jhon Anderson Alonso <programador2@impsercom.com.co>
2023-09-02 07:02:05 -0500

    creacion de modal sectionsheetmodal y cambio de ubicacion de sectionexternalsheetcontroller en donde logica de metodo store

---
+++ app/Http/Controllers/ModuleCms/SectionExternalSheetsController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\ModuleCms;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contents\SectionExternalSheets;
+use App\Http\Requests\StoreSectionExternalSheetsRequest;
+use App\Http\Requests\UpdateSectionExternalSheetsRequest;
+
+class SectionExternalSheetsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSectionExternalSheetsRequest $request)
+    {
+    try {
+            $input = $request->all();
+            // dd( $input);
+            $sectionExternalSheets = SectionExternalSheets::create($input);
+            return response()->json(['msg' => 'success', 'data' => $sectionExternalSheets,'status'=> 200]);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSectionExternalSheetsRequest $request, SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SectionExternalSheets $sectionExternalSheets)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/ModuleCMS/components/SectionSheetModal.vue
@@ -0,0 +1,181 @@
+<script setup>
+import axios from "axios";
+import { computed, onMounted, onUnmounted, ref, watch } from "vue";
+
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false,
+    },
+    maxWidth: {
+        type: String,
+        default: "2xl",
+    },
+    closeable: {
+        type: Boolean,
+        default: true,
+    },
+    actionModal: {
+        type: String,
+        default: null,
+    },
+    titleModal: {
+        type: String,
+        default: null,
+    },
+});
+
+const emit = defineEmits(["close"]);
+
+watch(
+    () => props.show,
+    () => {
+        if (props.show) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = null;
+        }
+    }
+);
+
+const close = () => {
+    if (props.closeable) {
+        emit("close");
+    }
+};
+const closeModal = () => {
+    // Emitir un evento para cerrar el modal
+    emit("close");
+};
+
+const closeOnEscape = (e) => {
+    if (e.key === "Escape" && props.show) {
+        close();
+    }
+};
+
+onMounted(() => document.addEventListener("keydown", closeOnEscape));
+
+onUnmounted(() => {
+    document.removeEventListener("keydown", closeOnEscape);
+    document.body.style.overflow = null;
+});
+
+const maxWidthClass = computed(() => {
+    return {
+        sm: "sm:max-w-sm",
+        md: "sm:max-w-md",
+        lg: "sm:max-w-lg",
+        xl: "sm:max-w-xl",
+        "2xl": "sm:max-w-2xl",
+    }[props.maxWidth];
+});
+
+const form = ref({
+    name: "",
+    description: "",
+});
+const sendForm = (event) => {
+    console.log("enviando");
+    axios.post('/admin/section_external_content',form.value)
+};
+</script>
+
+<template>
+    <div
+        class="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-70"
+    >
+        <div class="modal-overlay"></div>
+
+        <div
+            class="modal-container bg-gray-50 md:w-11/12 sm:w-full mx-auto rounded shadow-lg overflow-auto"
+        >
+            <div class="modal-header bg-purple-100 py-2 pl-8 pr-2 w-full">
+                <h2 class="text-2xl font-semibold">{{ titleModal }}</h2>
+                <button @click="closeModal" class="modal-close">
+                    <i class="fa-regular fa-circle-xmark fa-lg"></i>
+                </button>
+            </div>
+
+            <div class="modal-body mb-2">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <div class="my-1">
+                        <label
+                            for="name_section"
+                            class="block mb-2 text-sm font-medium text-gray-900"
+                            >name</label
+                        >
+                        <input
+                            type="text"
+                            id="name_section"
+                            v-model="form.name"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            placeholder="name"
+                            required
+                        />
+                    </div>
+                    <div class="my-1">
+                        <label
+                            for="description"
+                            class="block mb-2 text-sm font-medium text-gray-900"
+                            >description
+                        </label>
+                        <input
+                            type="text"
+                            id="description"
+                            v-model="form.description"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            placeholder="description"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer bg-yellow-400 pt-4 flex px-4">
+                <button
+                    @click="closeModal"
+                    class="relative inline-flex items-center text-red-800 justify-center mb-2 mr-2 overflow-hidden text-md font-medium px-5 py-2.5 transition-all ease-in duration-75 bg-white border-2 border-red-600 hover:text-red-600 rounded-md hover:border-3 hover:bg-opacity-50"
+                >
+                    Cerrar
+                </button>
+                <button
+                    @click="sendForm"
+                    class="relative inline-flex items-center text-blue-800 justify-center mb-2 mr-2 overflow-hidden text-md font-medium px-5 py-2 transition-all ease-in duration-75 bg-white border-2 border-blue-600 hover:text-blue-600 rounded-md hover:border-3 hover:bg-opacity-50"
+                >
+                    <i class="fas fa-save fa-2x"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* Estilos Tailwind para el modal */
+.modal-overlay {
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-container {
+    max-height: 90vh;
+}
+
+.modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.modal-body {
+    padding: 10px 20px;
+}
+
+.modal-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: sticky;
+    bottom: 0;
+}
+
+.modal-close {
+    cursor: pointer;
+}
+</style>
